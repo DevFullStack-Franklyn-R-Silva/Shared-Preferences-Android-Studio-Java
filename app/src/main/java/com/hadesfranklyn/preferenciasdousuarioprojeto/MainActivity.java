373af32130aa45d170fd480d8d23b4fbe4f49365
Franklyn R <franklyn1998@live.com>
2022-09-08 11:39:46 -0300

    Shared Preferences

--- app/src/main/java/com/hadesfranklyn/preferenciasdousuarioprojeto/MainActivity.java
+++ app/src/main/java/com/hadesfranklyn/preferenciasdousuarioprojeto/MainActivity.java
@@ -1,14 +1,66 @@
 package com.hadesfranklyn.preferenciasdousuarioprojeto;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String ARQUIVO_PREFERENCIA = "ArquivoPreferencia";
+    private Button buttonSalvar;
+    private TextInputEditText editNome;
+    private TextView textResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSalvar = findViewById(R.id.buttonSalvar);
+        editNome = findViewById(R.id.editNome);
+        textResultado = findViewById(R.id.textResultado);
+
+        buttonSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SharedPreferences preferences = getSharedPreferences(ARQUIVO_PREFERENCIA, 0);
+                SharedPreferences.Editor editor = preferences.edit();
+
+                //Validar o nome
+                if (editNome.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "Preencha o nome", Toast.LENGTH_LONG).show();
+                } else {
+
+                    String nome = editNome.getText().toString();
+                    editor.putString("nome", nome);
+                    //Salva o dados
+                    editor.commit();
+
+                    //Exibir o resultado
+                    textResultado.setText("Olá, " + nome);
+                }
+            }
+        });
+
+        //Recuperar dados salvos
+        SharedPreferences preferences = getSharedPreferences(ARQUIVO_PREFERENCIA, 0);
+
+        //Valida se temos o nome em preferencias
+        if(preferences.contains("nome")){
+
+            String nome = preferences.getString("nome", "usuário não definido");
+            textResultado.setText("Olá, " + nome);
+
+        }else{
+            textResultado.setText("Olá, usuário não definido");
+        }
     }
 }
